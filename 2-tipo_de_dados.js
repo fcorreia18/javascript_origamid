0ae9abe23a7e8774e5dce1de12a1fe39aaec3a1a
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-30 14:37:03 +0100

    aula 2 tipos primitivos de dados

--- 2-tipo_de_dados.js
+++ 2-tipo_de_dados.js
@@ -1,3 +1,12 @@
 //Existem 7 tipos de dados primitivos em javascript
 
 //Todos são primitivos excepto os objectos
+
+let nome = "Francisco" //string
+let idade = 22 //number
+let temCarro = true //boolean
+let simbolo = Symbol() //symbol
+let dataDeViagem; //undefined
+let vaiViajar = null //null
+let semDados = empty //empty
+let abilidades = {} //object
