1be5bb341d224f224e23c4be02d0f3a54bea5da4
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-30 15:07:26 +0100

    Resolvendo exercícios para praticar a usabilidade dos tipos de dados

--- 2-tipo_de_dados.js
+++ 2-tipo_de_dados.js
@@ -25,3 +25,34 @@ console.log(numero + +mes)
 let texto = "Fui ao trabalho no decorrer do feriado";
 
 console.log(`Ontem ${texto}`)
+
+
+
+
+
+
+
+
+
+
+
+//EXERCÍCIOS
+
+/*
+
+1 declare uma variável contendo uma  string
+2 declare uma variável contendo um número dentro de uma string
+3 declare uma variável com a sua idade
+4 declare duas variáveis uma com o seu nome e outra com o seu sobrenome e some as duas
+5 coloque a seguinte frase em uma variável: It's time;
+6 verifique o tipo de variável que contêm o seu nome;
+*/
+
+const teclado ="teclado numerico";
+let variavel_com_numero = "2"
+let minha_idade = 22;
+let primeiro_nome = "Francisco";
+let ultimo_nome = "Correia";
+console.log( primeiro_nome + ultimo_nome);
+let frase = "It's time"
+console.log(`${frase} de colocar uma expressão aqui`)
