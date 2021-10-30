58b4892cda167be7909f3b16d8a09e4cd516936c
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-30 15:42:06 +0100

    exemplos de operadores

--- 3-operadores.js
+++ 3-operadores.js
@@ -2,3 +2,13 @@
 
 let idade = 2e12;
 console.log(idade);
+
+
+//OPERADORES ARITMÉTICOS 
+
+let soma = 15 + 5;//20
+let subtracao = 15 - 5;//10
+let multiplicacao = 15*5;//75
+let divisao = 15 / 5;//3
+let expoente = 2 ** 4;//16
+let modulo = 14 % 5;// 4
