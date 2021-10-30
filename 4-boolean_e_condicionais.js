b896f3fca72b61c1f2b365b54a255aed6ad37c20
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-30 21:34:33 +0100

    valores que retornam false

--- 4-boolean_e_condicionais.js
+++ 4-boolean_e_condicionais.js
@@ -2,3 +2,13 @@
 
 //Existem valores que retornam truee outros que retornam
 // false quando verificados em uma expressão boleana
+
+
+//Falsy
+//toda expressão que tenha um dos valores abaixo resultará em false
+if (false){}
+if (0){}//ou -0
+if (NaN){}
+if (null){}
+if (null){}
+if (''){}// ou "" ``
