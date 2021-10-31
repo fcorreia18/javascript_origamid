77b622512266c018be641a3117cf6927a5d06160
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-31 10:22:55 +0100

    exercícios sobre objectos resolvidos

--- 6-objectos.js
+++ 6-objectos.js
@@ -15,3 +15,23 @@ let quadrado = {
 }
 
 console.log(quadrado.perimetro())
+
+
+//nomeie 3 proriedades ou metodos de string
+
+// endsWith
+// concat
+// includes
+// indexOf
+
+
+let div = document.querySelector('div');
+//after
+//append
+//onload
+//textContent
+//style
+
+div.addEventListener('copy', function(){
+    return console.log("ola")
+    });
