e675594854ddcea5bada485e302ee12b70d33110
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-31 01:51:24 +0100

    resolvendo mais exercícios

--- 5-funcoes.js
+++ 5-funcoes.js
@@ -1,4 +1,4 @@
-const Truthy = (valor)=>{
+const isTruthy = (valor)=>{
     if(!!valor){
         console.log("é Truthy")
     }else{
@@ -6,12 +6,43 @@ const Truthy = (valor)=>{
     }
 }
 
-Truthy("lsal 9");
+isTruthy("lsal 9");
 
-let square = (...lados)=>{
- let result =   lados.reduce((value, ac)=>{
-    return value + ac;
- }, 0)
-    return result;
+// let square = (...lados)=>{
+//  let result =   lados.reduce((value, ac)=>{
+//     return value + ac;
+//  }, 0)
+//     return result;
+// }
+//console.log(square(1, 9,1,1));~
+let square = (lado)=>{
+
+        return lado * 4;
+    }
+console.log(square(5))
+
+let meuNome = (primeiro_nome, ultimo_nome)=>{
+    return `${primeiro_nome} ${ultimo_nome}`;
 }
-console.log(square(1, 9,1,1));
+
+meuNome("Francisco", "Correia");
+
+let isEven = (numero) =>{
+    if((numero % 2 )=== 0){
+        return "par"
+    }
+    return "impar"
+}
+
+console.log(isEven(20));
+
+
+let tipoDeDado = (dados)=>{
+    return `o tipo de dados é ${typeof dados}`;
+}
+
+console.log(tipoDeDado(true));
+
+addEventListener('scroll',()=>{
+    return console.log(meuNome("Francisco", "Correia"));
+})
