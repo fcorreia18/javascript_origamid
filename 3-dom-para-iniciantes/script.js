71fa1a72f4ae6611b2efd871744787441f8a28a2
Francisco Correia <francisco.correia1804@gmail.com>
2021-10-31 13:33:10 +0100

    praticando selecionando elementos

---
+++ 3-dom-para-iniciantes/script.js
@@ -0,0 +1,14 @@
+let imagens = document.querySelectorAll('ul.animais-lista li>img');
+console.log(imagens)
+
+let links = document.querySelectorAll("a[href^='#']");
+console.log(links);
+
+let animais = document.querySelector('.animais-descricao');
+console.log(animais.querySelector('h2'));
+
+let first_h2 = document.querySelectorAll('.animais-descricao h2')[0];
+console.log(first_h2);
+
+let last_p = document.querySelector('footer p');
+console.log(last_p)
