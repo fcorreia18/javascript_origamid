887f77ffe0efac82dbb439b47c627f1808ad9287
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-11 13:58:07 +0100

    exercício de codigo com erro

--- 3-dom-para-iniciantes/script-exercicio.js
+++ 3-dom-para-iniciantes/script-exercicio.js
@@ -1,3 +1,17 @@
 //mostre no console cada paragrafo do site
 const paragrafos = document.querySelectorAll('p');
-paragrafos.forEach(item => console.log(item.textContent));
+paragrafos.forEach(item => console.log(item.innerText));//or textContent
+
+
+//Como corrigir os erros abaixo?
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(item, index => {
+    console.log(item, index);
+})
+
+let i = 0;
+imgs.forEach(=>{
+    i++;
+})
