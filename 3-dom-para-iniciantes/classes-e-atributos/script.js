9b98e9b076633d4e9e9387d908a5c89087580cab
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-11 14:19:06 +0100

    manipulando classes

--- 3-dom-para-iniciantes/classes-e-atributos/script.js
+++ 3-dom-para-iniciantes/classes-e-atributos/script.js
@@ -1,4 +1,12 @@
 const menu = document.querySelector('.menu');
 console.log(menu);
 
+menu.classList.add('vermelho');
+menu.classList.add('branco','preto');
 console.log(menu.classList)//mostra a lista de classes do elemento
+menu.classList.remove('branco', 'preto')
+menu.classList.toggle('vermelho')
+menu.classList.replace('azul', 'outraclasse')
+menu.className += " nova classe";//maneira antiga de manipular classes
+console.log(menu.classList)
+
