2b1549f0fdd1baa066325a5241f0a562048aaa15
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-13 07:21:22 +0100

    dimensões e distâncias

--- 3-dom-para-iniciantes/dimensoes-e-distancias/script-exercicio.js
+++ 3-dom-para-iniciantes/dimensoes-e-distancias/script-exercicio.js
@@ -1,2 +1,6 @@
 const animais = document.querySelector('.animais-lista');
 console.log(animais.offsetTop);//A distância entre o topo do elemento e o topo da página
+console.log(animais.offsetLeft);//A distância entre o canto esquerdo do elemento e o canto esquerdo da página
+
+const rect = document.querySelector('h2');
+console.log(rect.getBoundingClientRect())
